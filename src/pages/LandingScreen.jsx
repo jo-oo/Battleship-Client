d32fc8cb87623167a6dadaf72ef4dd24e2833bee
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-05-16 14:56:52 +0200

    Cleanup and commenting

--- src/pages/LandingScreen.jsx
+++ src/pages/LandingScreen.jsx
@@ -1,33 +1,42 @@
-import { useEffect, useRef, useState } from 'react'
+// Import hooks from react
+import { useRef, useState } from 'react'
 
 const Landing = () => {
 
+  // State for if user is waiting for opponent
   const [isLoading, setIsLoading] = useState(false)
+  // State for name entered in form
   const [nameInput, setNameInput] = useState()
   const searchInputRef = useRef()
 
+  // When form is submitted
   const handleSubmit = (e) => {
     e.preventDefault()
+    // Set loading state to true
     setIsLoading(true)
-    console.log('Your name is:', nameInput )
   }
 
   return (
     <>
     {
+      // Show start screen form when loading state is false
       !isLoading && 
-      (<section className='start-screen'>
-          <h1>Battleship</h1>
-          <form className='form' onSubmit={handleSubmit}>
-            <input type="text" id="username" className="form-control form-control-lg" onChange={e => setNameInput(e.target.value)} ref={searchInputRef} placeholder="Your name" required autoFocus />
-            <br />
-            <button type="submit" className="btn btn-primary">Enter queue</button>
-          </form>
-      </section>)
+      (
+        <section className='start-screen'>
+            <h1>Battleship</h1>
+            <form className='form' onSubmit={handleSubmit}>
+              <input type="text" id="username" className="form-control form-control-lg" onChange={e => setNameInput(e.target.value)} ref={searchInputRef} placeholder="Your name" required autoFocus />
+              <br />
+              <button type="submit" className="btn btn-primary">Enter queue</button>
+            </form>
+        </section>
+      )
     }
 
     {
-      isLoading && (
+      // Show waiting screen when loading state is true
+      isLoading && 
+      (
         <section className='waiting-screen'>
           <p>Hello {nameInput}</p>
           <p>Waiting for opponent...</p>
