45cfa851b2e7d4135b30cb39dfe496a84a15cf16
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-05-17 16:43:00 +0200

    Ads possible structure for ships

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -12,8 +12,51 @@ const GameScreen = () => {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-  ;
+  // Array of ships to place
+  const ships = [
+    {
+      length: 2,
+      startPos: null,
+      coords: [],
+    },
+    {
+      length: 2,
+      startPos: null,
+      coords: [],
+    },
+    {
+      length: 3,
+      startPos: null,
+      coords: [],
+    },
+    {
+      length: 4,
+      startPos: null,
+      coords: [],
+    }
+  ]
 
+  // Function to set coordinates for all ships
+  const fillShipCoord = () => {
+
+    ships.forEach( (ship) => {
+      // Randomize ship starting position
+      ship.startPos = getRandomNumber(1, 11) * getRandomNumber(1, 11)
+
+      // Loop through index values from start position of ship up until it's length, and push values to it's coords array
+      for (let i = ship.startPos; i < ship.startPos + ship.length; i++) {
+        ship.coords.push(i)
+      }
+    } )
+    
+  }
+
+  fillShipCoord()
+
+  // Console log info about each ship to make sure it's correct
+  ships.forEach( (ship) => {
+    console.log('ship of length', ship.length, 'has coords', ship.coords)
+  } )
 
   let shipOne = getRandomNumber(1, 11) * getRandomNumber(1, 11)
   let shipTwo = getRandomNumber(1, 11) * getRandomNumber(1, 11)
