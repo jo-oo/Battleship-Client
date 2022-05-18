edd36fb7c0bef592ce9bdf3a5f0a38fd7f95843d
Maria Lundhammar <marialundhammar@gmail.com>
2022-05-18 13:11:08 +0200

    started with a random direction function

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -18,45 +18,81 @@ const GameScreen = () => {
       length: 2,
       startPos: null,
       coords: [],
+      direction: [1, 2, 3, 4]
     },
     {
       length: 2,
       startPos: null,
       coords: [],
+      direction: [1, 2, 3, 4]
     },
     {
       length: 3,
       startPos: null,
       coords: [],
+      direction: [1, 2, 3, 4]
     },
     {
       length: 4,
       startPos: null,
       coords: [],
+      direction: [1, 2, 3, 4]
     }
   ]
+
+
+
+
+
+
 
   // Function to set coordinates for all ships
   const fillShipCoord = () => {
 
-    ships.forEach( (ship) => {
+
+
+    ships.forEach((ship) => {
+      const randomDirection = Math.floor(Math.random() * 3);
       // Randomize ship starting position
       ship.startPos = getRandomNumber(1, 11) * getRandomNumber(1, 11)
 
-      // Loop through index values from start position of ship up until it's length, and push values to it's coords array
-      for (let i = ship.startPos; i < ship.startPos + ship.length; i++) {
-        ship.coords.push(i)
+      if (randomDirection === 0) {
+        console.log("This is random direction 0")
+        // Loop through index values from start position of ship up until it's length, and push values to it's coords array
+        for (let i = ship.startPos; i < ship.startPos + ship.length; i++) {
+          ship.coords.push(i)
+        }
+
       }
-    } )
-    
+      if (randomDirection === 1) {
+        console.log("This is random direction 1")
+        for (let i = ship.startPos; ship.coords.length < ship.length; i -= 1) {
+          ship.coords.push(i)
+        }
+      }
+      if (randomDirection === 2) {
+        console.log("This is random direction 2")
+        for (let i = ship.startPos; ship.coords.length < ship.length; i -= 10) {
+          ship.coords.push(i)
+        }
+      }
+      if (randomDirection === 3) {
+        console.log("This is random direction 3")
+        for (let i = ship.startPos; ship.coords.length < ship.length; i += 10) {
+          ship.coords.push(i)
+        }
+      }
+
+
+    })
+
   }
 
   fillShipCoord()
-
   // Console log info about each ship to make sure it's correct
-  ships.forEach( (ship) => {
+  ships.forEach((ship) => {
     console.log('ship of length', ship.length, 'has coords', ship.coords)
-  } )
+  })
 
   let shipOne = getRandomNumber(1, 11) * getRandomNumber(1, 11)
   let shipTwo = getRandomNumber(1, 11) * getRandomNumber(1, 11)
