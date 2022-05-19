6a1d4a6fc04f0eac8d4d1b6bd5b1e3ce207c06ce
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-05-19 10:41:05 +0200

    Changes placement of function to avoid repeated calls

--- src/pages/GameScreen.jsx
+++ src/pages/GameScreen.jsx
@@ -3,7 +3,51 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 
+//Function for random number
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
+// Array of ships to place
+const ships = [
+  {
+    length: 2,
+    startPos: null,
+    coords: [],
+  },
+  {
+    length: 2,
+    startPos: null,
+    coords: [],
+  },
+  {
+    length: 3,
+    startPos: null,
+    coords: [],
+  },
+  {
+    length: 4,
+    startPos: null,
+    coords: [],
+  }
+]
+
+// Function to set coordinates for all ships
+const fillShipCoord = () => {
+
+  ships.forEach( (ship) => {
+    // Randomize ship starting position
+    ship.startPos = getRandomNumber(1, 11) * getRandomNumber(1, 11)
+
+    // Loop through index values from start position of ship up until it's length, and push values to it's coords array
+    for (let i = ship.startPos; i < ship.startPos + ship.length; i++) {
+      ship.coords.push(i)
+    }
+  } )
+  
+}
+
+fillShipCoord()
 
 const GameScreen = ({opponent, player, shouldStart, socket}) => {
 
@@ -24,56 +68,7 @@ const GameScreen = ({opponent, player, shouldStart, socket}) => {
     }
   }
 
-  //Function for random number
-  function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
-  // Array of ships to place
-  const ships = [
-    {
-      length: 2,
-      startPos: null,
-      coords: [],
-    },
-    {
-      length: 2,
-      startPos: null,
-      coords: [],
-    },
-    {
-      length: 3,
-      startPos: null,
-      coords: [],
-    },
-    {
-      length: 4,
-      startPos: null,
-      coords: [],
-    }
-  ]
-
-  // Function to set coordinates for all ships
-  const fillShipCoord = () => {
-
-    ships.forEach( (ship) => {
-      // Randomize ship starting position
-      ship.startPos = getRandomNumber(1, 11) * getRandomNumber(1, 11)
-
-      // Loop through index values from start position of ship up until it's length, and push values to it's coords array
-      for (let i = ship.startPos; i < ship.startPos + ship.length; i++) {
-        ship.coords.push(i)
-      }
-    } )
-    
-  }
-
-  fillShipCoord()
-
-  // Console log info about each ship to make sure it's correct
-  ships.forEach( (ship) => {
-    console.log('ship of length', ship.length, 'has coords', ship.coords)
-  } )
+  
 
   let shipOne = getRandomNumber(1, 11) * getRandomNumber(1, 11)
   let shipTwo = getRandomNumber(1, 11) * getRandomNumber(1, 11)
@@ -88,6 +83,15 @@ const GameScreen = ({opponent, player, shouldStart, socket}) => {
     socket.on('game:click', handleOppClick)
 
   }, [socket] )
+
+  useEffect( () => {
+
+    // Console log info about each ship to make sure it's correct
+    ships.forEach( (ship) => {
+      console.log('ship of length', ship.length, 'has coords', ship.coords)
+    } )
+
+  }, [] )
 
   return (
 
