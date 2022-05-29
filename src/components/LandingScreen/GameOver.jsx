6cd97c49225f6d5ad57162789d7cbcc246dba38e
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-05-29 10:27:57 +0200

    GameOver component now uses correct prop names

--- src/components/LandingScreen/GameOver.jsx
+++ src/components/LandingScreen/GameOver.jsx
@@ -3,26 +3,26 @@
 */
 
 //takes in resultData and handleSubmit from GameScreen
-const GameOver = ( { resultData, handleSubmit} ) => {
+const GameOver = ( { result, submit} ) => {
 	return ( 
         <>
             <section className='result-screen'>
                 <div className='result-screen-wrapper'>
                     {
-                        resultData.won && (
+                        result.won && (
                             <>
                                 <div>Congratulations, you won!</div>
                             </>
                         )
                     }
                     {
-                        !resultData.won && (
+                        !result.won && (
                             <>
                                 <div>You lost the game :&#40;</div>
                             </>
                         )
                     }
-                    <button className="btn btn-primary" onClick={handleSubmit}>Play again</button>
+                    <button className="btn btn-primary" onClick={submit}>Play again</button>
                 </div>
             </section>  
         </> 
