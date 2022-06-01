8f5782667f025c5536b3b91ec1af1dbb1a5675ae
Maria Lundhammar <marialundhammar@gmail.com>
2022-06-01 11:27:52 +0200

    adding some styling to gameboard

--- src/components/GameScreen/HitOrMiss.jsx
+++ src/components/GameScreen/HitOrMiss.jsx
@@ -3,44 +3,43 @@
  */
 
 //takes in pixelArray, arrayOfHits et.c. from GameScreen
-const HitOrMiss = ({ pixelArray, arrayOfHits, arrayOfMissed, handleOppBoardClick, opponent }) => {
+const HitOrMiss = ({ pixelArray, arrayOfHits, arrayOfMissed, handleOppBoardClick }) => {
   return (
     <>
-      <div className='float-left'>
-        <h2> ⚓️{opponent} board</h2>
-      </div>
-      <div className='gameBoard'>
-        {pixelArray.map((pixel, index) => {
-          //render out the array containing hit coords for player (not opponent)
-          for (let i = 0; i < arrayOfHits.length; i++) {
-            if (arrayOfHits[i] === pixel.number) {
-              return (
-                <div className='pixelHit' key={index}>
-                  {pixel.number}
-                </div>
-              );
+      {pixelArray !== null && (
+        <div className='gameBoard'>
+          {pixelArray.map((pixel, index) => {
+            //render out the array containing hit coords for player (not opponent)
+            for (let i = 0; i < arrayOfHits.length; i++) {
+              if (arrayOfHits[i] === pixel.number) {
+                return (
+                  <div className='pixelHit' key={index}>
+                    {pixel.number}
+                  </div>
+                );
+              }
             }
-          }
 
-          //render out the array containing missed coords for player (not opponent)
-          for (let i = 0; i < arrayOfMissed.length; i++) {
-            if (arrayOfMissed[i] === pixel.number) {
-              return (
-                <div className='pixelMiss' key={index}>
-                  {pixel.number}
-                </div>
-              );
+            //render out the array containing missed coords for player (not opponent)
+            for (let i = 0; i < arrayOfMissed.length; i++) {
+              if (arrayOfMissed[i] === pixel.number) {
+                return (
+                  <div className='pixelMiss' key={index}>
+                    {pixel.number}
+                  </div>
+                );
+              }
             }
-          }
-          //if it's not hit or miss render just a normal clickable pixel
-          return (
-            //when clicking on this pixel handleOppBoardClick is running
-            <div className='pixel' onClick={() => handleOppBoardClick(pixel.number)} key={index}>
-              {pixel.number}
-            </div>
-          );
-        })}
-      </div>
+            //if it's not hit or miss render just a normal clickable pixel
+            return (
+              //when clicking on this pixel handleOppBoardClick is running
+              <div className='pixel' onClick={() => handleOppBoardClick(pixel.number)} key={index}>
+                {pixel.number}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
