203ad5b52f08c1e32d9109721b46ae960ce2de8f
jo-oo <johannakoreo@gmail.com>
2022-06-02 11:01:37 +0200

    Edit Start Screen

--- src/components/LandingScreen/StartScreen.jsx
+++ src/components/LandingScreen/StartScreen.jsx
@@ -3,17 +3,17 @@
  */
 
 //takes in setNameInput from GameScreen
-const StartScreen = ({ handleSubmit, setNameInput, searchInputRef  }) => {
+const StartScreen = ({ handleSubmit, setNameInput, searchInputRef }) => {
     return (
       <>
-        <div className='start-screen' >
+        <div xs={1} md={3} l={8} className='start-screen g-4' >
           <section  id='input-field'>
-              <h1>Battleship</h1>
-            <form className='form' onSubmit={handleSubmit}>
-              <input type="text" id="username" className="form-control form-control-lg" onChange={e => setNameInput(e.target.value)} ref={searchInputRef} placeholder="Your name" required autoFocus />
-              <br />
-              <button type="submit" className="btn btn-primary">Enter queue</button>
-            </form>
+              <h1>Battleship </h1>
+                <form className='form' onSubmit={handleSubmit}>
+                  <input type="text" id="username" className="form-control form-control-lg" onChange={e => setNameInput(e.target.value)} ref={searchInputRef} placeholder="Your name" required autoFocus />
+                  <br />
+                  <button type="submit" className="btn btn-primary">Enter queue</button>
+                </form>
           </section>
         </div>
       </>
